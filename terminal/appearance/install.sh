6f2fa8394c9040dce3495b2cc2b69e83fdd89cf7
Andreas Gravgaard Andersen <andreasga22@gmail.com>
2020-07-07 17:20:42 +0200

    Fix fonts for power- and airline

--- terminal/appearance/install.sh
+++ terminal/appearance/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+dotfiles_install_package ttf-dejavu-sans-mono-powerline-git
+
 dotfiles_install_remote_component GITHUB tlatsas/xcolors xcolors
 dotfiles_install_component .Xresources $HOME/.Xresources
 
