317f6e0cb73e47b341f9b3d16e35662a40959338
Andreas Gravgaard Andersen <andreasga22@gmail.com>
2020-07-07 13:58:07 +0200

    Fix package install args

--- i3/install.sh
+++ i3/install.sh
@@ -5,7 +5,9 @@ if [ ! -d $doti3 ]; then
     mkdir $doti3
 fi
 
-dotfiles_install_package i3 i3status dmenu
+dotfiles_install_package i3
+dotfiles_install_package i3status
+dotfiles_install_package dmenu
 
 dotfiles_install_component i3config $doti3/config
 dotfiles_install_component i3status.conf $HOME/.i3status.conf
